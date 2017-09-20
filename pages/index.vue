753c6ba8e645dd68973eeb0866ca3a1c49b8f5c5
billyryanwill <billy.williams@distinction.co.uk>
2017-09-20 17:58:35 +0100

    Initial commit via upload

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="grid">
+    <div>
+      <h1>Test</h1>
+      <router-link to="what-is">Next</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    moveToNextPage (page) {}
+  }
+}
+</script>
+
+<style>
+  .grid {
+
+        display:grid;
+        grid-template-columns:repeat(16,1fr);
+        grid-template-rows:repeat(auto-fill, 64px);
+
+    }
+</style>
---
+++ pages/what-is.vue
@@ -0,0 +1,45 @@
+<template>
+
+        <div class="grid">
+            <div class="title">
+                <h1>What is CSS Grid?</h1>
+                <button v-on:keyup.enter="transition"></button>
+            </div>
+        </div>
+    
+</template>
+
+<script>
+
+export default {
+  created: function () {
+    
+  },
+  methods: {
+    transition: () => {
+      console.log(this.$router.push('/'))
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+    .grid {
+
+        display:grid;
+        grid-gap:16px;
+        grid-template-columns:repeat(16,1fr);
+        grid-template-rows:repeat(auto-fill, 64px);
+
+    }
+
+    .title {
+
+        grid-column-start:3;
+        grid-column-end:span 10;
+        grid-row-start:6;
+
+    }
+
+</style>
